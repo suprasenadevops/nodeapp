f9f3cf8d7f281e3541b3b49b4b73fe046976f8c0
suprasenadevops <suprasenadevopstraining@gmail.com>
2024-06-04 20:29:46 +0530

    Update server.js
    Changeing the text

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-     res.send("Welcome to my awesome app!"); 
+     res.send("Welcome to my awesome app!. this is awesome"); 
  });
 
 app.listen(3000, function () {
